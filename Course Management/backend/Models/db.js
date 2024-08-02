3cab1205125f24cb6132b5f3c5e39ba4c3cf4c66
Ranvirkar <87645357+Ranvirkar@users.noreply.github.com>
2024-08-02 10:42:40 +0530

    Add files via upload

---
+++ Course Management/backend/Models/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', // Replace with your MySQL username
+    password: 'root', // Replace with your MySQL password
+    database: 'course_management', // Replace with your database name
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL Database');
+});
+
+module.exports = db;
